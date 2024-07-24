db8d68c93b0d37ed106ad6719707c8e1ada60ed6
Rengga <renggarendy22@gmail.com>
2024-07-24 11:39:15 +0700

    feat: register

---
+++ src/components/views/auth/Register/index.tsx
@@ -0,0 +1,133 @@
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
+
+const RegisterView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { push } = useRouter();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError("");
+    const form = event.target as HTMLFormElement;
+    const data = {
+      email: form.email.value,
+      fullname: form.fullname.value,
+      phone: form.phone.value,
+      password: form.password.value,
+    };
+
+    const result = await fetch("/api/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (result.status === 200) {
+      form.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError("Email is already registered");
+    }
+  };
+
+  return (
+    <div className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Create an account
+            </h1>
+            {error && <p className="text-red-600 font-medium">{error}</p>}
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="name@company.com"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="fullname"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Fullname
+                </label>
+                <input
+                  type="text"
+                  name="fullname"
+                  id="fullname"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="Insert your name here"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="fullname"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Phone
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="Insert your phone here"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                {isLoading ? "Loading..." : "Create an account"}
+              </button>
+            </form>
+            <p>
+              Have an account? Sign in{" "}
+              <Link
+                href={"/auth/login"}
+                className="text-blue-700 font-medium cursor-pointer hover:underline"
+              >
+                here
+              </Link>{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterView;
---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,12 @@
+import LoginView from "@/components/views/auth/Login";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <LoginView />
+    </>
+  );
+};
+
+export default RegisterPage;
